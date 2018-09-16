1dfb005ade0ffe30a2c62b9f0bf557d9b1bf9431
Gaby Lamoureux <glamoureux@my.bcit.ca>
2018-09-15 22:51:50 -0700

    Added tests.

--- BitShift/main.cpp
+++ BitShift/main.cpp
@@ -6,6 +6,12 @@ int main() {
     std::cout << a << " shifted left by " << b << " = " << c << std::endl;
     a = 3, b = 7, c = bitshift (a, b, true);
     std::cout << a << " shifted left by " << b << " = " << c << std::endl;
+    a = 20, b = 2, c = bitshift (a, b, false);
+    std::cout << a << " shifted right by " << b << " = " << c << std::endl;
+    a = 5, b = 0, c = bitshift (a, b, false);
+    std::cout << a << " shifted right by " << b << " = " << c << std::endl;
+    a = 0, b = 13, c = bitshift (a, b, false);
+    std::cout << a << " shifted right by " << b << " = " << c << std::endl;
     return 0;
 }
 
