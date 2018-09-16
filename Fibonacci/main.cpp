a5a81c0af8065a54652de7ba7342ce8222dd5059
Gaby Lamoureux <glamoureux@my.bcit.ca>
2018-09-15 22:30:30 -0700

    There was no problem I just forgot to enter arguments. 10/10.

--- Fibonacci/main.cpp
+++ Fibonacci/main.cpp
@@ -1,24 +1,14 @@
 #include <iostream>
-#include <cstring>
 
 int fib(int a);
-int a = 0;
 
 int main(int argc, char *argv[]) {
-    std::cout << "Before search" << std::endl;
-    int search = atoi(argv[1]);
-    std::cout << "After search" << std::endl;
-    std::cout << "Before fib" << std::endl;
-
-//    std::cout << "Fibonacci number at position " << search << " is: " << fib(search) << std::endl;
-    std::cout << "After fib" << std::endl;
-
+    int s = std::stoi(argv[1]);
+    std::cout << "Fibonacci number " << s << " is: " << fib(s) << std::endl;
     return 0;
 }
 
 int fib(int a){
-    a += 1;
-    std::cout << a << std::endl;
     if (a == 1 || a == 2)
         return 1;
     else
