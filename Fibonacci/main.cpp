9adf57faf40b2745212c6d6b3a6bd2086b7511c7
Gaby Lamoureux <glamoureux@my.bcit.ca>
2018-09-15 22:10:36 -0700

    Testing arg input - works.

--- Fibonacci/main.cpp
+++ Fibonacci/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int fib(int a);
+
+int main(int argc, char *argv[]) {
+    std::cout << argv[1] << std::endl;
+//    int search = *argv[1];
+
+//    std::cout << "Fibonacci number at position " << search << " is: " << fib(search) << std::endl;
     return 0;
+}
+
+int fib(int a){
+    if (a == 1 || a == 2)
+        return 1;
+    else
+        return fib(a-1) + fib (a-2);
 }
