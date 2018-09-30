8bcf9780f9096d3adcf369f911467415f0528177
Gaby Lamoureux <glamoureux@my.bcit.ca>
2018-09-29 18:11:14 -0700

    Added random generator for number of ints and for the doubles.

--- Lab2Activities/ToQorNotToQ/main.cpp
+++ Lab2Activities/ToQorNotToQ/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ifstream fin;
+    std::string word;
+    int count = 0;
+    fin.open("Hamlet.txt");
+    while (fin >> word)
+        count = count+1;
+    std::cout << count << " words." << std::endl;
+//    if (hamlet.is_open())
+//        std::cout << "Hello, World!" << std::endl;
+//    else
+//        std::cout << "O fuck" << std::endl;
+//    fin.close();
     return 0;
 }
