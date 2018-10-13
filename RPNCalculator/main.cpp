1e37be4a915fa591416b4086d760bde3c5aa3de6
Gaby Lamoureux <glamoureux@my.bcit.ca>
2018-10-12 17:55:53 -0700

    Finishing perform, starting process_formula.

--- RPNCalculator/main.cpp
+++ RPNCalculator/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include "cmake-build-debug/CMakeFiles/rpn_calculator.hpp"
 
 int main() {
     std::cout << "Enter your formula:\n";
     std::string formula;
     std::getline(std::cin, formula);
     std::cout << "You entered " << formula << std::endl;
-    RPNCalculator calculator;
+    rpn_calculator calculator;
     int result = calculator.process_formula(formula);
     std::cout << "The result is:\n";
     std::cout <<  result << std::endl;
